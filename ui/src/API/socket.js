c3d42d6deb7d9a7faffe416b6bc611a8b418920f
Spyware08 <0699gurpreet@gmail.com>
2026-01-29 11:43:44 +0530

    first commit

--- ui/src/API/socket.js
+++ ui/src/API/socket.js
@@ -1,10 +1,4 @@
-// import { io } from "socket.io-client";
 
-// const socket = io("http://192.168.31.107:8000", {
-//   autoConnect: false,
-// });
-
-// export default socket;
 
 import { io } from "socket.io-client";
 const API_URL = import.meta.env.VITE_API_URL;
@@ -12,6 +6,7 @@ console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const socket = io(API_URL, {
   autoConnect: false,
+    transports: ["websocket"], 
 });
 
 export default socket;
